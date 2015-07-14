030602218382545976fca07981908d5018435b52
Fokko Driesprong <fokko@driesprongen.nl>
2015-07-14 11:10:06 +0200

    Created a test script to produce results

---
+++ doTest.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+a=1
+
+
+while [ $a -lt 11 ]
+do
+   docker-compose kill
+   docker-compose rm -f
+   docker-compose up -d master zookeeper kafka worker
+   docker-compose scale worker=$a
+   
+   sec=`expr $a * 5`
+   sleep $sec
+
+   docker-compose run task
+
+   a=`expr $a + 1`
+done
