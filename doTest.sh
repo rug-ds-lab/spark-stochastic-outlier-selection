b1da17227c4fac4062307db1516a4e3217182d2e
Fokko Driesprong <fokko@driesprongen.nl>
2015-07-20 14:47:38 +0200

    fix

--- doTest.sh
+++ doTest.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
+
 a=1
+b=10
 
-
-while [ $a -lt 11 ]
+while [ $b -lt 11 ]
 do
-   docker-compose kill
-   docker-compose rm -f
-   docker-compose up -d master zookeeper kafka worker
-   docker-compose scale worker=$a
+	while [ $a -lt 21 ]
+	do
+	   docker-compose kill
+	   docker-compose rm -f
+	   docker-compose up -d master zookeeper kafka worker
+	   docker-compose scale worker=$a
    
-   sec=`expr $a \* 5`
-   sleep $sec
+	   sec=`expr $a \* 5`
+	   sleep $sec
 
-   docker-compose run task
+	   docker-compose run task /usr/spark/bin/spark-submit --class com.quintor.EvaluateOutlierDetectionDistributed /tmp/app/target/scala-2.10/QuintorSparkOutlier-assembly-1.0.jar 2000 /tmp/results/test$a.txt
 
-   a=`expr $a + 1`
+	   a=`expr $a + 1`
+	done
+b=`expr $b + 1`
 done
