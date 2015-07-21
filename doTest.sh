ba77825cca1f3d78b5d74c59d3acfc3104b2d11c
Fokko Driesprong <fokko@driesprongen.nl>
2015-07-21 11:48:23 +0200

    merge

--- doTest.sh
+++ doTest.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 
-a=1
+a=3
 b=1
 
 topic="OutlierObservations"
-
-sbt assembly
 
 while [ $b -lt 11 ]
 do
@@ -19,8 +17,8 @@ do
    		sec=`expr $a \* 5`
    		sleep $sec
 
-        docker-compose run kafka /opt/kafka_2.10-0.8.2.1/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partition $a --topic $topic
-		docker-compose run task /usr/spark/bin/spark-submit --class com.quintor.EvaluateOutlierDetectionDistributed --master spark://master:7077 /tmp/app/target/scala-2.10/QuintorSparkOutlier-assembly-1.0.jar 1000 $a $topic /tmp/results/output.txt
+		docker-compose run kafka /opt/kafka_2.10-0.8.2.1/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partition $a --topic $topic
+		docker-compose run task /usr/spark/bin/spark-submit --class com.quintor.EvaluateOutlierDetectionDistributed --master spark://master:7077 /tmp/app/target/scala-2.10/QuintorSparkOutlier-assembly-1.0.jar 1000 $a $topic /tmp/results/final.txt
 
 		a=`expr $a + 1`
 	done
