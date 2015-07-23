32855a9c09ffae2595d59a856e912802c3b98588
Fokko Driesprong <fokko@driesprongen.nl>
2015-07-23 16:03:35 +0200

    fix

--- run-local.sh
+++ run-local.sh
@@ -5,23 +5,25 @@ docker-compose rm -f
 
 docker-compose scale master=1 zookeeper=1 kafka=1 worker=1
 
-a=8
+workers=8
 n=500
 
-while [ $a -lt 11 ]
+while [ $workers -lt 11 ]
 do
     b=1
     while [ $b -lt 6 ]
     do
-        docker-compose scale kafka=$a worker=$a
+        partitions=`expr $workers \* 3`
 
-        docker-compose run kafka /opt/kafka_2.10-0.8.2.1/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partition $a --topic OutlierObservations$a
+        docker-compose scale kafka=$workers worker=$workers
 
-        docker-compose run generator sbt "run $n $a OutlierObservations$a"
+        docker-compose run kafka /opt/kafka_2.10-0.8.2.1/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partition $workers --topic OutlierObservations$workers
 
-        docker-compose run task /usr/spark/bin/spark-submit --class com.quintor.EvaluateOutlierDetectionDistributed --master spark://master:7077 /tmp/app/target/scala-2.10/QuintorSparkOutlier-assembly-1.0.jar $n $a 4 OutlierObservations$a /tmp/results/scaling.txt
+        docker-compose run generator sbt "run $n $workers $partitions OutlierObservations$workers"
 
-        docker-compose run kafka /opt/kafka_2.10-0.8.2.1/bin/kafka-topics.sh --zookeeper zookeeper:2181 --delete --topic OutlierObservations$a
+        docker-compose run task /usr/spark/bin/spark-submit --class com.quintor.EvaluateOutlierDetectionDistributed --master spark://master:7077 /tmp/app/target/scala-2.10/QuintorSparkOutlier-assembly-1.0.jar $n $workers $partitions OutlierObservations$workers /tmp/results/scaling.txt
+
+        docker-compose run kafka /opt/kafka_2.10-0.8.2.1/bin/kafka-topics.sh --zookeeper zookeeper:2181 --delete --topic OutlierObservations$workers
 
         b=`expr $b + 1`
     done
