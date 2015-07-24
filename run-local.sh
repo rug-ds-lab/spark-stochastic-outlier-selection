ca062453f8db8b300ba18fdbdf45b1f2a05e6554
Fokko Driesprong <fokko@driesprongen.nl>
2015-07-24 17:31:10 +0200

    fixed script

--- run-local.sh
+++ run-local.sh
@@ -11,24 +11,25 @@ workers=1
 n=500
 c=1
 
+docker-compose scale kafka=$workers worker=$workers
+
+
 while [ $workers -lt 11 ]
 do
     b=1
+
+    echo "Creating topic with $part partitions"
+
+    docker-compose run kafka /opt/kafka_2.10-0.8.2.1/bin/kafka-topics.sh \
+                --create \
+                --zookeeper zookeeper:2181 \
+                --replication-factor 1 \
+                --partition $part \
+                --topic OutlierObservations$c
+
     while [ $b -lt 11 ]
     do
         part=`expr $workers \* 3`
-
-	echo "Creating topic with $part partitions"
-
-        docker-compose scale kafka=$workers worker=$workers
-
-        docker-compose run kafka /opt/kafka_2.10-0.8.2.1/bin/kafka-topics.sh \
-                    --create \
-                    --zookeeper zookeeper:2181 \
-                    --replication-factor 1 \
-                    --partition $part \
-                    --topic OutlierObservations$c
-
 
         docker-compose run generator sbt "run $n $workers $part OutlierObservations$c"
 
@@ -37,14 +38,15 @@ do
                                     --master spark://master:7077 /tmp/app/target/scala-2.10/QuintorSparkOutlier-assembly-1.0.jar \
                                     $n $workers $part OutlierObservations$c /tmp/results/yay.txt
 
-        docker-compose run kafka /opt/kafka_2.10-0.8.2.1/bin/kafka-topics.sh \
-                        --zookeeper zookeeper:2181 \
-                        --delete \
-                        --topic OutlierObservations$c
-
         b=`expr $b + 1`
-	c=`expr $c + 1`
     done
+
+    docker-compose run kafka /opt/kafka_2.10-0.8.2.1/bin/kafka-topics.sh \
+                    --zookeeper zookeeper:2181 \
+                    --delete \
+                    --topic OutlierObservations$c
+
+    c=`expr $c + 1`
     n=`expr $n \* 2`
 done
 
