9a0b3511f6380d952ce1d081fc9bbc96bf0153cc
Fokko Driesprong <fokko@driesprongen.nl>
2015-08-31 19:52:26 +0200

    works now

--- run-stats.sh
+++ run-stats.sh
@@ -5,9 +5,8 @@ docker-compose rm -f
 
 docker-compose up -d master zookeeper kafka worker history
 
-sleep 3
 
-workers=4
+workers=1
 n=100
 c=1
 part=12
@@ -18,6 +17,7 @@ rm -rf ./events
 mkdir ./events
 chmod -R 777 ./events
 
+sleep 2
 docker-compose scale kafka=$workers worker=$workers
 sleep 10
 
@@ -32,13 +32,12 @@ docker-compose run kafka /opt/kafka_2.10-0.8.2.1/bin/kafka-topics.sh \
 
 docker-compose run generator sbt "run $n $workers $part OutlierObservations"
 
-while [ $b -lt 11 ]
-do
+#while [ $b -lt 11 ]
+#do
     docker-compose run task /usr/spark/bin/spark-submit \
                                 --class com.quintor.EvaluateOutlierDetectionDistributed \
                                 --master spark://master:7077 /tmp/app/target/scala-2.10/QuintorSparkOutlier-assembly-1.0.jar \
                                 $n $workers $part OutlierObservations /tmp/results/yay12.txt
 
     b=`expr $b + 1`
-done
 
